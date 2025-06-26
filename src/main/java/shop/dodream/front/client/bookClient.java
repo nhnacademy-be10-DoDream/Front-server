ce3fc1d05c9a37a209fe86eb7cdd178d012f639c
bagjinhwan <spdlqjxp@gmail.com>
2025-06-26 09:18:03 +0900

    feat: 스타일 추가
    #7

---
+++ src/main/java/shop/dodream/front/client/bookClient.java
@@ -0,0 +1,12 @@
+package shop.dodream.front.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "bookClient", url = "dodream.shop:10325")
+public class bookClient {
+    @GetMapping("/books")
+    public String getBooks() {
+        return "";
+    }
+}
