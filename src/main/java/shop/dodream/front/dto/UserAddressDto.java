dc61605a082f37c482acc10381308f65ade274ca
bagjinhwan <spdlqjxp@gmail.com>
2025-06-27 13:38:04 +0900

    feat: 주소 생성 dto 추가
    #16

---
+++ src/main/java/shop/dodream/front/dto/UserAddressDto.java
@@ -0,0 +1,13 @@
+package shop.dodream.front.dto;
+
+import lombok.Data;
+
+@Data
+public class UserAddressDto {
+    private String alias; // 주소별칭
+    private String postcode; // 우편번호
+    private String roadAddress; // 도로명주소
+    private String jibunAddress; // 지번주소
+    private String extraAddress; // 참고항목
+    private String detailAddress; // 상세주소
+}
