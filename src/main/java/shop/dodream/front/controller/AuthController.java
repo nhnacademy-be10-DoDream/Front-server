17b5b38d7f26ba6bf79344bdea6b553b38c0f672
HyunJin <guswlsdl7771@naver.com>
2025-06-30 17:51:54 +0900

    fix: redirect url

--- src/main/java/shop/dodream/front/controller/AuthController.java
+++ src/main/java/shop/dodream/front/controller/AuthController.java
@@ -66,7 +66,7 @@ public class AuthController {
 
     @GetMapping("/payco/login")
     public String paycoLogin() {
-        return "redirect:http://localhost:10320/auth/payco/authorize";
+        return "redirect:http://s1.jav21.net/auth/payco/authorize";
     }
 
     @GetMapping("/payco/callback")
