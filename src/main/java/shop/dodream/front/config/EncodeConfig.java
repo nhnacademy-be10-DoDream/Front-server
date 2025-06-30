231753c09c6ed743a12c7b49fc892e0b2bcbd64a
bagjinhwan <spdlqjxp@gmail.com>
2025-06-30 14:52:42 +0900

    feat: 비밀번호 인코딩 빈 추가
    #29

---
+++ src/main/java/shop/dodream/front/config/EncodeConfig.java
@@ -0,0 +1,14 @@
+package shop.dodream.front.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class EncodeConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
