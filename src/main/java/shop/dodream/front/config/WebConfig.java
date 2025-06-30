81a1bd0d91c26380a451d798441e752cfdcdaf54
bagjinhwan <spdlqjxp@gmail.com>
2025-06-30 14:52:05 +0900

    feat: 비밀번호 인코딩 설정 및 빈 추가
    #29

--- src/main/java/shop/dodream/front/config/WebConfig.java
+++ src/main/java/shop/dodream/front/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/auth/login-form").setViewName("auth/login");
         registry.addViewController("/auth/signup-form").setViewName("auth/signup");
     }
