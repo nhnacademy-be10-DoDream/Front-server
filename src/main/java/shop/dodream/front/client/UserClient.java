345b22f89ffe24d374bcd6dad24ee3737414f42b
PARK JINHWAN <136469279+spdlqjxp@users.noreply.github.com>
2025-06-30 16:21:17 +0900

    merge: feat/34-after-login into develop (#35)
    * feat: userDto 추가
    
    #34
    
    * feat: User 정보 받아오는 메서드 추가
    
    #34
    
    * feat: 로그인 후 헤더 변경
    
    로그인, 회원가입 버튼 안보이게 변경
    
    #34
    
    * feat: 로그인 후 유저 정보 세션에 저장
    
    #34

--- src/main/java/shop/dodream/front/client/UserClient.java
+++ src/main/java/shop/dodream/front/client/UserClient.java
@@ -1,13 +1,8 @@
 package shop.dodream.front.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import shop.dodream.front.dto.CreateAccountRequest;
-import shop.dodream.front.dto.CreateAccountResponse;
-import shop.dodream.front.dto.SignupRequest;
-import shop.dodream.front.dto.UserAddressDto;
+import org.springframework.web.bind.annotation.*;
+import shop.dodream.front.dto.*;
 
 @FeignClient(name = "userClient", url = "http://localhost:10320")
 public interface UserClient {
@@ -18,4 +13,6 @@ public interface UserClient {
     @PostMapping("/users/{user-id}/address")
     void createUserAddress(@PathVariable("user-id") String userId, @RequestBody UserAddressDto userAddressDto);
 
+    @GetMapping("/users/me")
+    UserDto getUser(@CookieValue("accessToken") String accessToken);
 }
---
+++ src/main/java/shop/dodream/front/dto/UserDto.java
@@ -0,0 +1,19 @@
+package shop.dodream.front.dto;
+
+import lombok.Data;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+public record UserDto(
+        String userId,
+        String email,
+        String name,
+        String phone,
+        String status,
+        Date birthDate,
+        ZonedDateTime createdAt,
+        ZonedDateTime lastLoginAt,
+        String grade,
+        long point
+) {}
