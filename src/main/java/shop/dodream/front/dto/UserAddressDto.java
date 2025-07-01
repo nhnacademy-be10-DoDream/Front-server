af5deb6489e1508cc2983b88ccfe0756ffbbb839
bagjinhwan <spdlqjxp@gmail.com>
2025-07-01 15:26:49 +0900

    feat: 회원 주소 dto 우편번호 필드명 변경
    #26

--- src/main/java/shop/dodream/front/dto/UserAddressDto.java
+++ src/main/java/shop/dodream/front/dto/UserAddressDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class UserAddressDto {
     private String alias; // 주소별칭
-    private String postcode; // 우편번호
+    private String zipcode; // 우편번호
     private String roadAddress; // 도로명주소
     private String jibunAddress; // 지번주소
     private String extraAddress; // 참고항목
