61856e2fcf4a5f997dbdfbac8da2b1abf386c89c
bagjinhwan <spdlqjxp@gmail.com>
2025-07-01 15:56:26 +0900

    feat: BookClient Http method 추가
    #26

--- src/main/java/shop/dodream/front/client/BookClient.java
+++ src/main/java/shop/dodream/front/client/BookClient.java
@@ -1,11 +1,13 @@
 package shop.dodream.front.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import shop.dodream.front.dto.BookDto;
 
 import java.util.List;
 
 @FeignClient(name = "bookClient", url = "http://localhost:10320")
 public interface BookClient {
+    @GetMapping("/books/internal/book-list")
     List<BookDto> bookList();
 }
