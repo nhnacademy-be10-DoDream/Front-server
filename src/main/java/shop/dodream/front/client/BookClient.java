2ec00fdcba444020a138cfdb43bcb9ad9bb43f1a
song-sang-jin <ssj30099@naver.com>
2025-07-02 17:33:40 +0900

    feat: 도서 상세페이지 html 로 하드코딩으로 구현
    #50

--- src/main/java/shop/dodream/front/client/BookClient.java
+++ src/main/java/shop/dodream/front/client/BookClient.java
@@ -2,6 +2,7 @@ package shop.dodream.front.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import shop.dodream.front.dto.BookDetailDto;
 import shop.dodream.front.dto.BookDto;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface BookClient {
     @GetMapping("/admin/books")
     List<BookDto> getBooks();
+
+//    @GetMapping("/public/books/{book-id}")
+//    BookDetailDto getBookD()
 }
--- src/main/java/shop/dodream/front/dto/BookDetailDto.java
+++ src/main/java/shop/dodream/front/dto/BookDetailDto.java
@@ -1,4 +1,24 @@
 package shop.dodream.front.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Data
 public class BookDetailDto {
+    private Long bookId;
+    private String title;
+    private String author;
+    private String description;
+    private String publisher;
+    private String isbn;
+    private LocalDate publishedAt;
+    private Long salePrice;
+    private Long regularPrice;
+    private Boolean isGiftable;
+    private List<String> bookUrls;
+    private Long discountRate;
+
 }
