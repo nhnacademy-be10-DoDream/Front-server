880d9388325978e2e65a846d1da116c310dae393
bagjinhwan <spdlqjxp@gmail.com>
2025-07-03 16:22:14 +0900

    feat: 인터셉터 추가
    #40

--- src/main/java/shop/dodream/front/config/WebConfig.java
+++ src/main/java/shop/dodream/front/config/WebConfig.java
@@ -1,15 +1,23 @@
 package shop.dodream.front.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import shop.dodream.front.interceptor.LoginUserInterceptor;
+import shop.dodream.front.interceptor.RequestInterceptor;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserInterceptor loginUserInterceptor;
+    private final RequestInterceptor requestInterceptor;
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
@@ -21,8 +29,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/auth/login-form").setViewName("auth/login");
         registry.addViewController("/auth/signup-form").setViewName("auth/signup");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginUserInterceptor);
+        registry.addInterceptor(requestInterceptor);
     }
 }
