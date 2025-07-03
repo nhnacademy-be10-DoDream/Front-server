1b8d1d558b6f9abcbb665eb33ee34e226ec9a2e6
bagjinhwan <spdlqjxp@gmail.com>
2025-07-03 16:16:14 +0900

    feat: 클라이언트 Url 변경
    #40

--- src/main/java/shop/dodream/front/client/OrderClient.java
+++ src/main/java/shop/dodream/front/client/OrderClient.java
@@ -1,17 +1,18 @@
 package shop.dodream.front.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import shop.dodream.front.dto.OrderRequest;
 import shop.dodream.front.dto.OrderResponse;
 import shop.dodream.front.dto.ShippingPolicyDto;
 
 import java.util.List;
+import java.util.Map;
 
 @FeignClient(name = "orderClient", url = "http://localhost:10320")
 public interface OrderClient {
-//    @PostMapping("/orders")
-//    Map<String, Object> createOrder(@RequestBody OrderRequest orderRequest);
+    @PostMapping("/orders")
+    Map<String, Object> createOrder(@RequestBody OrderRequest orderRequest);
 //
 //    @GetMapping("/orders/gift-wrap")
 //    List<WrappingDto> getGiftWraps();
@@ -20,12 +21,11 @@ public interface OrderClient {
 //    WrappingDto getGiftWrapById(@PathVariable("gift-wrap-id") Long giftWrapId);
 
     @GetMapping("/orders/shipping")
-    List<ShippingPolicyDto> getShippingPolicies(@CookieValue("accessToken")String accessToken);
-    // Assuming you want to fetch shipping policies as well
+    List<ShippingPolicyDto> getShippingPolicies();
 
     @GetMapping("/orders")
-    List<OrderResponse> getOrders(@CookieValue("accessToken") String accessToken); // 주문 목록 조회
-
+    List<OrderResponse> getOrders(); // 주문 목록 조회
+//
 //    @GetMapping("/orders/{order-id}")
 //    OrderDetailsDto getOrderDetail(@PathVariable("order-id") String orderId); // 주문 상세 조회
 //
--- src/main/java/shop/dodream/front/client/UserClient.java
+++ src/main/java/shop/dodream/front/client/UserClient.java
@@ -9,42 +9,35 @@ import shop.dodream.front.dto.*;
 
 import java.util.List;
 
-//@FeignClient(name = "userClient", url = "https://localhost:10320")
-@FeignClient(name = "userClient", url = "s1.java21.net:10320")
+@FeignClient(name = "userClient", url = "https://localhost:10320")
 public interface UserClient {
-    @PostMapping("/users/signup")
+    @PostMapping("/public/users/signup")
     void createUserAccount(SignupRequest signupRequest);
 
     @GetMapping("/users/me")
-    UserDto getUser(@CookieValue("accessToken") String accessToken);
+    UserDto getUser();
 
     @PutMapping("/users/me")
-    void updateUser(@CookieValue("accessToken") String accessToken,
-                    @RequestBody UserUpdateDto userUpdateDto);
+    void updateUser(@RequestBody UserUpdateDto userUpdateDto);
 
     @PostMapping("/users/me/addresses")
-    void createUserAddress(@CookieValue("accessToken") String accessToken,
-                           @RequestBody UserAddressDto userAddressDto);
+    void createUserAddress(@RequestBody UserAddressDto userAddressDto);
 
     @PutMapping("/users/me/addresses/{address-id}")
-    void updateUserAddress(@CookieValue("accessToken") String accessToken,
-                           @PathVariable("address-id") Long addressId,
+    void updateUserAddress(@PathVariable("address-id") Long addressId,
                            @RequestBody UserAddressDto userAddressDto);
 
     @GetMapping("/users/me/addresses")
-    List<UserAddressResponse> getAddresses(@CookieValue("accessToken") String accessToken);
+    List<UserAddressResponse> getAddresses();
 
     @DeleteMapping("/users/me/addresses/{address-id}")
-    ResponseEntity<Void> deleteAddress(@CookieValue("accessToken") String accessToken,
-                                       @PathVariable("address-id") Long addressId);
+    ResponseEntity<Void> deleteAddress(@PathVariable("address-id") Long addressId);
 
     @GetMapping("/users/me/point-histories")
-    Page<PointHistoryResponse> getPointHistories(@CookieValue("accessToken") String accessToken,
-                                                 @RequestParam("page") int page,
+    Page<PointHistoryResponse> getPointHistories(@RequestParam("page") int page,
                                                  @RequestParam("size") int size,
                                                  @RequestParam(value = "sort", required = false) Sort sort);
 
     @GetMapping("/users/me/point-histories/{point-history-id}")
-    PointHistoryResponse getPointHistory(@CookieValue("accessToken") String accessToken,
-                                         @PathVariable("point-history-id") Long pointHistoryId);
+    PointHistoryResponse getPointHistory(@PathVariable("point-history-id") Long pointHistoryId);
 }
