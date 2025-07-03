0b2baa1b50634cd7984a0db6fd93c94140d18614
gunheecha <gunhee2326@naver.com>
2025-07-03 17:16:28 +0900

    feat: cart connection
    - Api Cart 서비스의 모든 로직을 받아와 컨트롤러에서 처리하는 로직을 추가, 컨트롤러의 사항을 반영해 뷰 페이지를 수정, 장바구니를 회원 장바구니와 비회원 장바구니로 분리, 비회원 장바구니 관련 dto 추가, 장바구니 화면에 전체 금액 계사하는 부분 추가, 장바구니 구분하는 로직 미구현
    
    #51

--- src/main/java/shop/dodream/front/client/CouponClient.java
+++ src/main/java/shop/dodream/front/client/CouponClient.java
@@ -2,6 +2,8 @@ package shop.dodream.front.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import shop.dodream.front.dto.CouponResponse;
 
@@ -10,6 +12,11 @@ import java.util.List;
 @FeignClient(name = "coupon", url = "http://localhost:10320")
 public interface CouponClient {
 	
-	@GetMapping("/coupon")
+	@GetMapping("/coupons")
 	List<CouponResponse> getAvailableCoupons(@RequestParam Long bookId, @RequestParam Long salePrice);
+	
+	@PutMapping("/coupons/me/{couponId}/apply")
+	void putCouponStatus(@PathVariable("couponId") Long couponId);
+	
+	
 }
