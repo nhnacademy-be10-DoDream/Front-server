a4894921006165217010433a13fcdb09168f4f05
gunheecha <gunhee2326@naver.com>
2025-07-08 13:10:48 +0900

    merge: feat/89-separate-cart into develop (#98)
    * feat: seperate cart
    
    - 쿠폰 응답 dto 이름변경
    
    #89
    
    * feat: seperate cart
    
    - 장바구니 페이지 변경
    
    #89
    
    * feat: seperate cart
    
    - 장바구니 요청 client 수정
    
    #89
    
    * feat: separate cart
    
    - 장바구니 Controller 수정
    
    #89
    
    * feat: separate cart
    
    - 쿠폰 dto 변경에 따른 요청 client 메서드 변경
    
    #89
    
    * feat: separate cart
    
    - 비회원 장바구니 페이지 수정
    
    #89
    
    * feat: separate cart
    
    - 비회원 장바구니 페이지 수정
    
    #89
    
    * feat: separate cart
    
    - 장바구니 페이지 수정
    
    #89
    
    * feat: separate cart
    
    - 장바구니 컨트롤러 url 수정
    
    #89
    
    * feat: separate cart
    
    - 장바구니 컨트롤러에 기능 통합으로 인한 삭제
    
    #89

--- src/main/java/shop/dodream/front/client/CouponClient.java
+++ src/main/java/shop/dodream/front/client/CouponClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import shop.dodream.front.dto.CouponResponse;
+import shop.dodream.front.dto.BookAvailableCouponResponse;
 
 import java.util.List;
 
@@ -13,7 +13,7 @@ import java.util.List;
 public interface CouponClient {
 	
 	@GetMapping("/coupons")
-	List<CouponResponse> getAvailableCoupons(@RequestParam Long bookId, @RequestParam Long salePrice);
+	List<BookAvailableCouponResponse> getBookAvailableCoupons(@RequestParam Long bookId, @RequestParam Long salePrice);
 	
 	@PutMapping("/coupons/me/{couponId}/apply")
 	void putCouponStatus(@PathVariable("couponId") Long couponId);
