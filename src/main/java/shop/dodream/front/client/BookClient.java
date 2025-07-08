d88a946d55ceb3a7922c47d3f00cd7f5777e7cf4
MonaLisaSy <ugs628@naver.com>
2025-07-08 13:14:55 +0900

    feat: nav tag
    #95

--- src/main/java/shop/dodream/front/client/BookClient.java
+++ src/main/java/shop/dodream/front/client/BookClient.java
@@ -2,7 +2,6 @@ package shop.dodream.front.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
