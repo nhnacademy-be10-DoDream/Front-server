ccb82a9895a11d41f1f743e76fc32d2dc11b5d93
gunheecha <gunhee2326@naver.com>
2025-07-09 14:01:19 +0900

    feat: update cart
    - 회원 장바구니 기능 추가
    
    #90

--- src/main/java/shop/dodream/front/client/CouponClient.java
+++ src/main/java/shop/dodream/front/client/CouponClient.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import shop.dodream.front.dto.AvailableCouponResponse;
 import shop.dodream.front.dto.BookAvailableCouponResponse;
 
