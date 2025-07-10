18737d430f5ea21c24916a966bce61f1708ff74d
HJin <guswlsdl7771@naver.com>
2025-07-10 13:24:31 +0900

    feat: CookieUtil 수정
    - CookieUtil 수정
    
    #137

--- src/main/java/shop/dodream/front/util/CookieUtils.java
+++ src/main/java/shop/dodream/front/util/CookieUtils.java
@@ -4,9 +4,6 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CookieUtils {
 
     public static String extractAccessCookie(HttpServletRequest request) {
