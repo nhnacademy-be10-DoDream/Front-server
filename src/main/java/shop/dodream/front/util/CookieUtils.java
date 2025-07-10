5cbcedc7c92f96aad22fa01a141272291ab34446
HJin <guswlsdl7771@naver.com>
2025-07-10 13:24:17 +0900

    feat: CookieUtil 수정
    - CookieUtil 수정
    
    #137

--- src/main/java/shop/dodream/front/util/CookieUtils.java
+++ src/main/java/shop/dodream/front/util/CookieUtils.java
@@ -9,16 +9,6 @@ import java.util.List;
 
 public class CookieUtils {
 
-    public static String extractCookieValue(List<String> cookies, String name) {
-        if (cookies == null || name == null) return null;
-        for (String cookie : cookies) {
-            if (cookie.startsWith(name + "=")) {
-                return cookie.split("=")[1].split(";")[0];
-            }
-        }
-        return null;
-    }
-
     public static String extractAccessCookie(HttpServletRequest request) {
         if(request.getCookies() != null) {
             for (Cookie cookie : request.getCookies()) {
@@ -47,18 +37,6 @@ public class CookieUtils {
         cookie.setPath("/");
         cookie.setMaxAge(0);
         response.addCookie(cookie);
-    }
-
-
-    public static List<String> convertToSetCookieList(Cookie[] cookies) {
-        List<String> list = new ArrayList<>();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                list.add(cookie.getName() + "=" + cookie.getValue());
-            }
-        }
-        return list;
-
     }
 
     public static void setCookie(HttpServletResponse response, String name, String value, int maxAgeSeconds, boolean httpOnly) {
