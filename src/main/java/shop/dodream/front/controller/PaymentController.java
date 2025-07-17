5c3aff844815ca9d7fe16bae7bf43fb536d753d9
PARK JINHWAN <136469279+spdlqjxp@users.noreply.github.com>
2025-07-17 12:30:05 +0900

    merge: feat/198-payment-processing into develop (#199)
    * refactor: 스크립트 분리
    
    #198
    
    * feat: 결제 요청시 결제 제공자 추가
    
    #198
    
    * feat: 결제 중 페이지 구현
    
    #198
    
    * feat: 결제 결과 dto 추가
    
    #198
    
    * feat: 결제 스크립트 결제중 페이지로 분리
    
    #198
    
    * feat: 주문 아이템 css 추가
    
    #198
    
    * feat: 결제 스크립트 분리
    
    #198

--- src/main/java/shop/dodream/front/controller/PaymentController.java
+++ src/main/java/shop/dodream/front/controller/PaymentController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import shop.dodream.front.client.CartClient;
 import shop.dodream.front.client.PaymentClient;
 import shop.dodream.front.dto.CartResponse;
+import shop.dodream.front.dto.PaymentResultDto;
 
+import java.util.Arrays;
 import java.util.Map;
 
 @Controller
@@ -20,7 +22,8 @@ public class PaymentController {
     private final PaymentClient paymentClient;
     private final CartClient cartClient;
     private final CartController cartController;
-    @RequestMapping(method = RequestMethod.GET)
+
+    @GetMapping
     public String checkOut() {
         return "order/payment/checkout";
     }
@@ -28,8 +31,9 @@ public class PaymentController {
     @RequestMapping(value = "/confirm")
     @ResponseBody
     public ResponseEntity<Map<String ,Object>> confirmPayment(@RequestBody String jsonBody,HttpServletRequest request,
-                                                              @RequestHeader(value = "X-USER-ID", required = false) String userId) {
-        Map<String ,Object> response = paymentClient.confirm(jsonBody, userId);
+                                                              @RequestHeader(value = "X-USER-ID", required = false) String userId,
+                                                              @RequestHeader(value = "X-PAYMENT-PROVIDER", required = false) String paymentProvider) {
+        Map<String ,Object> response = paymentClient.confirm(jsonBody, userId, paymentProvider);
         int statusCode = response.containsKey("error") ? 400 : 200;
         String guestId = cartController.getGuestIdFromCookie(request);
         if(guestId == null){
@@ -42,6 +46,11 @@ public class PaymentController {
         return ResponseEntity.status(statusCode).body(response);
     }
 
+    @GetMapping("process")
+    public String processingPayment() {
+        return "order/payment/processing";
+    }
+
     @GetMapping("/fail")
     public String failPayment(HttpServletRequest request, Model model) {
         model.addAttribute("errorCode", request.getParameter("code"));
@@ -49,11 +58,13 @@ public class PaymentController {
         return "order/payment/fail";
     }
 
-    @RequestMapping(value = "/success", method = RequestMethod.GET)
-    public String successPayment(HttpServletRequest request, Model model) {
-        model.addAttribute("code", request.getParameter("code"));
-        model.addAttribute("message", request.getParameter("message"));
-
+    @GetMapping("/success")
+    public String successPayment(@ModelAttribute PaymentResultDto result, Model model) {
+        model.addAttribute("orderId", result.getOrderId());
+        model.addAttribute("amount", result.getAmount());
+        model.addAttribute("method", result.getMethod());
+        model.addAttribute("address", result.getAddress());
+        model.addAttribute("items", Arrays.asList(result.getItems().split(";")));
         return "order/payment/success";
     }
 }
