412a73c03585350a0b56ce1b094b225ae8f869a2
bagjinhwan <spdlqjxp@gmail.com>
2025-07-17 14:16:22 +0900

    feat: 주석 제거
    #201

--- src/main/java/shop/dodream/front/dto/ShippingPolicyDto.java
+++ src/main/java/shop/dodream/front/dto/ShippingPolicyDto.java
@@ -4,9 +4,9 @@ import lombok.Data;
 
 @Data
 public class ShippingPolicyDto {
-    private Long id = 1L; // 배송 정책 ID
-    private String name; // 배송 정책 이름
-    private Integer fee; // 배송비
-    private Integer minimumAmount; // 최소 주문 금액
-    private boolean isActive; // 활성화 여부
+    private Long shippingId;
+    private String name;
+    private Integer fee;
+    private Integer minimumAmount;
+    private boolean isActive;
 }
