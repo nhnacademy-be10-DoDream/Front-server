933eaa8e7ea82e2345355a5fe378127e45a97d85
Geonwoo <163082451+GW000@users.noreply.github.com>
2025-07-21 16:20:05 +0900

    merge: feat/223-enhance-review-access into develop (#226)
    - 500 에러가 아닌 적절한 피드백 메세지 제공
    - MethodArgumentNotValidEx 에 대한 advice 핸들러 추가
    
    #223

--- src/main/java/shop/dodream/front/advice/GlobalExceptionHandler.java
+++ src/main/java/shop/dodream/front/advice/GlobalExceptionHandler.java
@@ -2,11 +2,29 @@ package shop.dodream.front.advice;
 
 import feign.FeignException;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.List;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handelMethodArgumentNotValid(MethodArgumentNotValidException e,
+                                               Model model) {
+        BindingResult bindingResult = e.getBindingResult();
+
+        List<String> errorMessage = bindingResult.getFieldErrors().stream()
+                .map(error -> String.format("[%s] : %s", error.getField(), error.getDefaultMessage()))
+                .toList();
+
+        model.addAttribute("errorMessages", errorMessage);
+
+        return "error/400";
+    }
 
     @ExceptionHandler(FeignException.class)
     public String handleFeign(FeignException e, Model model) {
