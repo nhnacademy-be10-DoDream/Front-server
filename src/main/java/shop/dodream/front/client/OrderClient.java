e0fc218266e215a569bb7972c0c1d64fc0799b22
bagjinhwan <spdlqjxp@gmail.com>
2025-07-23 14:58:33 +0900

    feat: 회원 비회원 페이징 처리
    #243

--- src/main/java/shop/dodream/front/client/OrderClient.java
+++ src/main/java/shop/dodream/front/client/OrderClient.java
@@ -8,7 +8,8 @@ import shop.dodream.front.dto.*;
 import java.util.List;
 import java.util.Map;
 
-@FeignClient(name = "orderClient", url = "${gateway.url}")
+//@FeignClient(name = "orderClient", url = "${gateway.url}")
+@FeignClient(name = "orderClient", url = "http://localhost:10327")
 public interface OrderClient {
     @PostMapping("/orders")
     Map<String, Object> createOrder(@RequestBody OrderRequest orderRequest);
@@ -33,7 +34,7 @@ public interface OrderClient {
     void deleteShippingPolicy(@PathVariable("shipping-policy-id") Long shippingPolicyId);
 
     @GetMapping("/orders")
-    List<OrderResponse> getOrders(); // 주문 목록 조회
+    Page<OrderResponse> getOrders(@RequestParam int page, @RequestParam int size); // 주문 목록 조회
 
     @GetMapping("/public/orders/{order-id}")
     OrderDetailsDto getOrderDetail(@PathVariable("order-id") String orderId); // 주문 상세 조회
@@ -48,7 +49,7 @@ public interface OrderClient {
     Map<String, Object> createOrderForGuest(OrderRequest orderRequest); // 비회원 주문 생성
 
     @PostMapping("/public/orders/guest")
-    List<OrderResponse> getOrdersForGuest(@RequestBody GuestAuthRequest guestAuthRequest); // 비회원 주문 목록 조회
+    Page<OrderResponse> getOrdersForGuest(@RequestBody GuestAuthRequest guestAuthRequest, @RequestParam int page, @RequestParam int size); // 비회원 주문 목록 조회
 
     @GetMapping("/admin/orders/order-status/{order-status}")
     Page<OrderByStatusResponse> getOrdersByStatus(@PathVariable("order-status") String status, @RequestParam int page, @RequestParam int size);
