e81017910547f76bc22e02d599c5bf6a474eb021
Geonwoo <163082451+GW000@users.noreply.github.com>
2025-07-24 09:30:15 +0900

    merge: feat/249-improve-review-accessibility into develop(#252)

--- src/main/java/shop/dodream/front/dto/ReviewCreateRequest.java
+++ src/main/java/shop/dodream/front/dto/ReviewCreateRequest.java
@@ -1,13 +1,16 @@
 package shop.dodream.front.dto;
 
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class ReviewCreateRequest {
-
-
+    @NotNull
+    @Min(0)
+    @Max(5)
     private Short rating;
+    @NotBlank
+    @Size(min = 5)
     private String content;
-
 }
